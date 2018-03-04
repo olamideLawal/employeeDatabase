f59c97cb3f0650bd8c7f35c40d290f05c6029b38
Ola <olamidelawal290@yahoo.co.uk>
2018-03-04 15:35:42 +0000

    added to " insert, update " to employee dao

--- httpServer/src/httpServer/EmployeeDAO.java
+++ httpServer/src/httpServer/EmployeeDAO.java
@@ -93,4 +93,73 @@ public class EmployeeDAO {
 		}
 		
 	}
+
+/**
+ * displayEmployees() gets the data from Employees table and makes an ArrayList of Employee object.
+ *  
+ * @return ArrayList of Employee Object.
+ * @throws SQLException
+ */
+
+public ArrayList<Employee> displayEmployees() throws SQLException{
+	
+	ArrayList<Employee> empList = new ArrayList<>();
+	
+	if(conn == null){
+		getConnection();
+	}
+	
+	try{
+	stmt = conn.createStatement();
+	res = stmt.executeQuery("SELECT * FROM Employees");
+	
+	
+	while(res.next()){
+		Employee employee = new Employee(res.getString("FirstName"),
+				res.getString("LastName"),res.getString("Gender"), res.getString("NIN"), 
+				res.getString("DOB"), res.getString("Address"), res.getString("Postcode"),
+				res.getString("Salary"),res.getString("StartDate"),res.getString("JobTitle"), 
+				res.getString("Email"));
+		empList.add(employee);
+	}
+	
+	return empList;
+	
+	}catch(SQLException se){
+	      //Handle errors for JDBC
+	      se.printStackTrace();
+	   }
+	
+		return empList;
+	}
+
+/**
+ * insertEmp method takes an instance of Employee and creates a new record in the database.
+ * 
+ * @param emp
+ * @throws SQLException
+ */
+
+public void insertEmp(Employee emp) throws SQLException {
+	if(conn == null){
+		getConnection();
+	}
+	try {
+		
+		String sql = "INSERT INTO Employees (FirstName, LastName, Gender, NIN, DOB, Address, Postcode, Salary, StartDate, JobTitle, Email) " +
+		"VALUES ( \"" +emp.getFirstName()+"\",\""+emp.getLastName()+"\", \""+emp.getGender()+"\", \""+emp.getNin()+"\", \""+
+				emp.getDob()+"\", \""+emp.getAddress()+"\", \""+emp.getPostcode()+"\", \""+emp.getSalary()+"\", \""+emp.getStartDate()+"\", \""+
+				emp.getJobTitle()+"\", \""+emp.getEmail()+"\");";
+
+	
+		stmt = conn.createStatement();
+		stmt.executeUpdate(sql);
+		System.out.println("Employee Records Successfully Created!\n");	
+		
+	} catch (SQLException e) {
+		e.printStackTrace();
+	}
+	
 }
+}
+
